ca70ac365d91199c64c29bfd5e6c315d2d3eaf80
rick <contato@joov.com.br>
2025-01-04 15:47:33 -0300

    refactor: edit routes

--- src/controllers/tests/index.ts
+++ src/controllers/tests/index.ts
@@ -14,13 +14,3 @@ export const listUsersController = async (req: Request, res: Response) => {
     }
 }
 
-// export const listUsersHistoryUpdateController = async (req: Request, res: Response) => {
-//     const users_history_update = await getTableData('users_history_update');
-//     res.json({ mensagem: 'List users history update', users_history_update });
-// }
-
-// export const listUsersHistoryDeleteController = async (req: Request, res: Response) => {
-//     const users_history_delete = await getTableData('users_history_delete');
-//     res.json({ mensagem: 'List users history delete', users_history_delete });
-// }
-
